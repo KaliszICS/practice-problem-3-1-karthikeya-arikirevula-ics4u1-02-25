b5eec7c7644a37cae0e99eaf0e4679bb83768e1a
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-03 23:11:07 +0000

    passed through mvn test

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,22 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int k) {
+
+			if(k < 0)
+				k*=-1;
+			if(k < 10)
+				return k;
+			return sumOfDigits(k / 10) + k % 10;
+		
+	}
+	public static int factorial(int a) {
+		if(a == 0)
+			return 1;
+		else if(a < 0)
+			return 0;
+		
+		return a * factorial(a-1);
+	}
 
 }
